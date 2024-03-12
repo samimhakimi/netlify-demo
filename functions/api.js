e21b190fe8dee18667cd129e9ed47419cf16a62a
Samim <samimhakimi11@gmail.com>
2024-03-12 11:04:34 +0000

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -3,12 +3,45 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
+const os = require('os');
+const si = require('systeminformation');
+
+
 let records = [];
 
 //Get all students
 router.get('/', (req, res) => {
   res.send('App is running..');
 });
+
+router.get("/sysinfo", (req, res) => {
+  res.json({
+
+// Get the serial number of the machine
+const serialNumber = os.cpus()[0].serial;
+
+// Display the serial number to the user
+console.log(`Your serial number is: ${serialNumber}`);
+
+  });
+});
+
+
+router.get("/sysinformation", (req, res) => {
+  res.json({
+// Get the serial number of the machine
+     si.system().then(data => {
+    const serialNumber = data.serial;
+    // Display the serial number to the user
+    console.log(`Your serial number is: ${serialNumber}`);
+}).catch(error => {
+    console.error(`Error retrieving serial number: ${error}`);
+});
+
+
+  });
+});
+
 
 //Create new record
 router.post('/add', (req, res) => {
